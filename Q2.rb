e0b4598341d1282f466539e236d6d9a25efe4c10
Yao <yao.engineering@gmail.com>
2013-04-23 14:36:04 -0400

    refactor Q2

--- Q2.rb
+++ Q2.rb
@@ -1,11 +1,23 @@
-for i in 1 .. 39
-	
-	int = i / 10
-	remainder = i % 10
-	if ( int > 0 )
-		for index in 1 .. int
-			print "X"
-		end
+
+
+	def main int, remainder
+		# for i in 1 .. 39
+			
+		# 	int = i / 10
+		# 	remainder = i % 10
+			if ( int > 0 )
+				for index in 1 .. int
+					print "X"
+				end
+				remainder_cal(remainder)
+			else
+				remainder_cal(remainder)
+			end
+		
+		# end
+	end
+
+	def remainder_cal remainder 
 		if ( remainder == 5 )
 			print "V "
 		elsif (remainder == 9 )
@@ -24,25 +36,10 @@ for i in 1 .. 39
 			end
 			print " "
 		end 
-	else
-		if ( remainder == 5 )
-			print "V "
-		elsif (remainder == 9 )
-			print "IX "
-		elsif (remainder > 5)
-			print "V"
-			for index in 1 .. remainder - 5
-				print "I"
-			end
-			print " "
-		elsif (remainder == 4) 
-			print "IV "
-		else
-			for index in 1 .. remainder
-				print "I"
-			end
-			print " "
-		end	
 	end
-end
 
+	for i in 1 .. 39
+		int = i / 10
+		remainder = i % 10
+		main(int, remainder)
+	end
